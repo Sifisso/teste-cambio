fd8a1f7c74e0fa14c38e71ec9406f33dd77ad3c8
Sifisso <sifissom@gmail.com>
2022-08-20 11:30:27 +0200

    Criação do projecto

---
+++ src/main/java/com/technoplus/TesteCambio/TesteCambioApplication.java
@@ -0,0 +1,13 @@
+package com.technoplus.TesteCambio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCambioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCambioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technoplus/TesteCambio/TesteCambioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technoplus.TesteCambio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCambioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
